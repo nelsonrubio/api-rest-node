a4756649cf3aa956c579e2864b117301ffce668a
Nelson Rubio <nelsonrubio20@gmail.com>
2018-06-13 09:52:33 -0400

    Listando server

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express'),
+    app = express(),
+    http = require('http'),
+    server = http.createServer(app),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override'),
+    mongoose = require('mongoose');
+    
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+var router = express.Router();
+
+router.get('/', function(req, res){
+    res.send("Hola mundo");
+});
+
+app.use(router);
+
+mongoose.connect('mongodb://localhost/tvshows', function(err, res) {
+  if(err) {
+    console.log('ERROR: connecting to Database. ' + err);
+  }
+  app.listen(3000, function() {
+    console.log("Node server running on http://localhost:3000");
+  });
+});
